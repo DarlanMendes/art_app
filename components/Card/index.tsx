919932b161b15bcb15bea9b40eb0b6b692cbbc30
Darlan Mendes <dedsondarlan@gmail.com>
2023-06-13 02:29:31 -0300

    Ajuste do card da arte e ajuste do tailiwind

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,51 +1,40 @@
+
 import Link from "next/link"
+import { IconContext } from "react-icons"
+import { IoIosArrowForward } from 'react-icons/io'
 
-interface Props{
-    id:string,
-    title:string,
-    author:string,
-    image:string
+interface Props {
+    id: string,
+    title: string,
+    author: string,
+    image: string,
+    fontSize: string
 }
+export default function Card(props: Props) {
+    console.log(props.fontSize)
+    return (
+        <Link href={`/artdetailed/${props.id}`}>
+            <div className="h-max-56 mt-8" >
+                <img src={props.image}
+                    alt={props.title}
+                    style={{ height: "calc((124/288) * (100vw - 32px))", width: "100%", objectFit: "cover" }}
+                />
+                <div className="p-4 flex flex-col ">
+                    <span className={`${props.fontSize === 'medium' ? 'text-sm' : 'text-medium'} mb-2`} >Artista</span>
+                    <div className="flex flex-row items-center justify-between">
+                        <h2 className={`basis-11/12 font-semibold ${props.fontSize === 'medium' ? "text-xl" : "text-2xl"}`}>
+                            {props.author} - {props.title}
+                        </h2>
+                        <IconContext.Provider
+                            value={props.fontSize === 'medium' ? { className: "text-xl mx-0 basis-1/12 flex flex-col items-center" } :
+                                { className: "text-2xl mx-0 basis-1/12 flex flex-col items-center" }}>
+                            <IoIosArrowForward />
+                        </IconContext.Provider>
+                    </div>
+                </div>
+            </div>
+        </Link>
 
-
-export default function Card(props:Props){
-    return(
-        <div>
-            <Link href={`ArtDetailed/${props.id}`}>
-            Card  - joão pedro
-            </Link>
-            
-        </div>
     )
 }
 
-
-import Link from "next/link"
-import {IoIosArrowForward} from 'react-icons/io'
-
-
-
-export default function Card ({ image, artista, nomeElemento, obraElemento })  {
-return (
-<>
-    <div className="max-w-[390px] m-[16px] max-h-[300px] flex flex-col items-start  ">
-    <div>
-        <img src={image} alt="image" className="h-[124px] w-[100vw] " />
-    </div>
-
-    <p className="pl-2 mt-2 text-[15px] font-medium">{artista}</p>
-
-    <div className="flex flex-col items-start pl-2 text-[25px] font-semibold  w-full max-w-[380px]  ">
-        <div>
-            <h2 className="whitespace-normal text-left ">
-            {nomeElemento} - {obraElemento}
-            </h2>
-        </div>
-        <span className=" text-[25px]">
-        <IoIosArrowForward />
-        </span>
-    </div>
-    </div>
-</>
-)
-}
