95a6ebd2d02adea04ccdec013a4222476fa88d49
Darlan Mendes <dedsondarlan@gmail.com>
2023-06-16 02:59:20 -0300

    Criação do contexto para o tema

---
+++ Context/Theme.tsx
@@ -0,0 +1,20 @@
+'use client'
+import { createContext, useState } from "react";
+
+interface ThemeContextType{
+    theme:string,
+    setTheme: React.Dispatch<React.SetStateAction<string>>
+}
+interface ThemeProviderProps{
+    children:React.ReactNode;
+}
+export const ThemeContext = createContext<ThemeContextType>({theme:'light', setTheme:()=>{}})
+
+export const ThemeProvider: React.FC<ThemeProviderProps>=({children})=>{
+    const [theme, setTheme]= useState<string>('light')
+    return(
+        <ThemeContext.Provider value={{theme, setTheme}}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 
+import { ThemeContext } from '../../Context/Theme'
 import Card from '../../components/Card'
 import Header from '../../components/Header'
-import {  useState } from 'react'
+import {  useContext, useState } from 'react'
 
 
 
@@ -12,22 +13,17 @@ export default function Home() {
   const[fontSize, setFontSize] = useState('medium')
 
 
-  const [theme, setTheme] = useState('light')
+  const {theme, setTheme} = useContext(ThemeContext)
 
-  // useEffect(() => {
-  //   if (hasCookie('theme')) {
-  //     setTheme((getCookie('theme') as string))
-  //   }
-  // }, [theme])
-
+ 
 
   return (
-    <div className=''>
+    <div className={`h-screen ${theme == 'light' ? "bg-white text-black" :" bg-black text-white"} transition duration-700 ease-in-out`}>
       <Header theme={theme} setTheme={setTheme} />
       <main className={`w-screen px-4 mt-24 ${theme == 'light' ? "bg-white text-black" :" bg-black text-white"} transition duration-700 ease-in-out`}>
-       
-        <h1>Bem-vindo!</h1>
-        <p>Siga um tour pelo o que é mostrado no museu pelo display do celular</p>
+       <hr className={`w-2/12 h-[3px] my-2 ${theme === 'light'?'bg-black':'bg-white'}`}/>
+        <h1 className={`text-4xl font-bold`}>Bem-vindo!</h1>
+        <p className='text-base mt-2'>Siga um tour pelo o que é mostrado no museu pelo display do celular</p>
         {cards.map((card, index)=>(
           <Card key={index}
           id={card.id}
