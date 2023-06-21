4972686c80845afe91c46baac7f65c190086006b
IcaroBacchi <icarobacchi14@gmail.com>
2023-06-21 18:36:52 -0300

    ajuste de bg e tentativa de pegar o cookie theme

--- components/buttonFontSize/index.tsx
+++ components/buttonFontSize/index.tsx
@@ -1,10 +1,26 @@
-export default function ButtonFontSize(){
+import { cookies } from 'next/headers'
+
+
+
+
+const getTheme = async ()=>{
+    let cookie = cookies()
+    return cookie.get('theme')
+}
+
+
+
+
+export default async function ButtonFontSize(){
+
+    const theme = await getTheme()
+console.log(theme)
 
     return(
         <>
-        <div id="sidebar" className="w-20   flex justify-center items-center">
-            <button  className="w-8 h-8 bg-teal-500 text-white select-none p-4 whitespace-nowrap rounded-tl-full rounded-bl-full  flex justify-center items-center  active:bg-teal-200">-a</button>
-            <button  className="w-8 h-8 bg-teal-500 text-white select-none p-4 whitespace-nowrap rounded-tr-full rounded-br-full  flex justify-center items-center   active:bg-teal-200 ">+A</button>
+        <div className={`${theme?.value ==='light'?'bg-white text-black':'bg-black text-white'} "w-20 flex justify-center items-center`} >
+            <button  className="w-8 h-8 bg-black text-white select-none p-4 whitespace-nowrap rounded-tl-full rounded-bl-full  flex justify-center items-center cursor-none md:cursor-pointer  active:bg-slate-800">-a</button>
+            <button  className="w-8 h-8 bg-black    text-white select-none p-4 whitespace-nowrap rounded-tr-full rounded-br-full  flex justify-center items-center cursor-none md:cursor-pointer active:bg-slate-800 ">+A</button>
         </div>
 
 
